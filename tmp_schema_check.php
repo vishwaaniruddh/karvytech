f0698945b3d06c78f31c36cb902d65003d10a76f
vishwaaniruddh <86979352+vishwaaniruddh@users.noreply.github.com>
2026-04-14 18:27:42 +0530

    deleted root md files

--- tmp_schema_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'c:/xampp/htdocs/project/config/database.php';
-$db = Database::getInstance()->getConnection();
-$stmt = $db->query('DESCRIBE dynamic_survey_fields');
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    print_r($row);
-}
-?>
